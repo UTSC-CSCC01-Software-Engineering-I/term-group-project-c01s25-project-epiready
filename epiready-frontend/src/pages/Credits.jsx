9f02e96db3dd31656086a6168237cd0f2d589c68
bduanmu <74875982+bduanmu@users.noreply.github.com>
2025-06-04 00:29:15 -0400

    feat: completed homepage including a navbar component

---
+++ epiready-frontend/src/pages/Credits.jsx
@@ -0,0 +1,7 @@
+export default function Credits() {
+  return (
+    <div>
+      <a href="https://www.flaticon.com/free-icons/syringe" title="syringe icons">Syringe icons created by Those Icons - Flaticon</a>
+    </div>
+  );
+}
