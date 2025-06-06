383073ef0e9a1cadcf49a0eca8039173903295ba
ansh-patel2005 <anshpat2005@gmail.com>
2025-06-06 12:17:03 -0400

    feat: completed database setup and created models

--- epiready-backend/app.py
+++ epiready-backend/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from routes.routes import blueprint
+from dotenv import load_dotenv
+from config.database import init_db
+
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = "SECRET_KEY"
 CORS(app)
 
-@app.route('/api/hello', methods=['GET'])
-def hello():
-    return jsonify({"message": "Hello from backend!"})
+init_db(app)
 
 app.register_blueprint(blueprint, url_prefix="/api/auth")
 
