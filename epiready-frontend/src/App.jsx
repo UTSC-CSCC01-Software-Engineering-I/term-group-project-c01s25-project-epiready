8a36e85af69e1be959c55ffc3e87e2cb736ce4af
bduanmu <74875982+bduanmu@users.noreply.github.com>
2025-06-08 21:11:25 -0400

    feat: added new pages and setup new styles for the navbar

--- epiready-frontend/src/App.jsx
+++ epiready-frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 import AuthForm from "./pages/AuthenticationForm";
 import Home from "./pages/Home";
 import Credits from "./pages/Credits";
+import Track from "./pages/Track";
+import Shipments from "./pages/Shipments";
+import Monitor from "./pages/Monitor";
+import Alerts from "./pages/Alerts";
 
 export default function App() {
   return (
@@ -11,6 +16,10 @@ export default function App() {
         <Route path="/login" element={<AuthForm type="login" />} />
         <Route path="/signup" element={<AuthForm type="signup" />} />
         <Route path="/credits" element={<Credits />} />
+        <Route path="/track" element={<Track />} />
+        <Route path="/shipments" element={<Shipments />} />
+        <Route path="/monitor" element={<Monitor />} />
+        <Route path="/alerts" element={<Alerts />} />
       </Routes>
     </BrowserRouter>
   );
