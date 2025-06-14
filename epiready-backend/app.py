dcf321f8dc24493d45cac018021644f4a0973113
shubhmgrg <shubham.garg@mail.utoronto.ca>
2025-06-14 10:25:41 -0400

    Integrated show shipments with backend to get data from the database

--- epiready-backend/app.py
+++ epiready-backend/app.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
-CORS(app)
+CORS(app, origins=os.getenv("CORS_ORIGIN"), supports_credentials=True)
 
 init_db(app)
 
--- epiready-frontend/src/components/Shipment/ShowShipments.jsx
+++ epiready-frontend/src/components/Shipment/ShowShipments.jsx
@@ -5,6 +5,7 @@ import styles from "./AddShipment.module.css";
 export default function ShowShipments() {
     const [shipments, setShipments] = useState([]);
     const [shipmentComponents, setShipmentComponents] = useState([]);
+    const [isLoading, setIsLoading] = useState(null);
 
     const addShipment = (
         <div className={styles.addShipment}>
@@ -15,14 +16,17 @@ export default function ShowShipments() {
     )
 
     useEffect(() => {
-        // Simulating fetching shipments data
-        setShipments([
-            { id: 4, name: "Shipment 3", location: "Location C", description: "Description C", statusCode: 3 },
-            { id: 1, name: "Shipment 1", location: "Location A", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dicta possimus voluptatum ipsum veritatis minus at blanditiis, ex, tenetur, dolorem accusamus nihil distinctio cupiditate dolores quidem illo assumenda illum! Reprehenderit.", statusCode: 0 },
-            { id: 2, name: "Shipment 2", location: "Location B", description: "Shipment is carryin 10000 kg of steel rods to show it in your mouth", statusCode: 1 },
-            { id: 3, name: "Shipment 3", location: "Location C", description: "Description C", statusCode: 2 },
-            
-        ]);
+        fetch("http://127.0.0.1:5000/api/shipments/", {
+        method: "GET",
+        headers: {
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE3NDk4NjY2NTAsImlhdCI6MTc0OTg2MzA1MH0.trvBfxsfH0GjJZMxEvPw829BQ5RwJJ77n9lqcYNRbfc',
+            'Content-Type': 'application/json'
+        }
+        })
+        .then((res) => {return res.json()})
+        .then(res => {
+            setShipments(res);
+        });
     }, []);
 
     useEffect(() => {
