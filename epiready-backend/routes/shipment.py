562fd33e889d267162d0d9015348b5c95e672903
SheepStar02 <wetmanroyale@gmail.com>
2025-06-21 13:35:58 -0400

    shipping api route change

--- epiready-backend/routes/shipment.py
+++ epiready-backend/routes/shipment.py
@@ -2,5 +2,5 @@ from flask import Blueprint
 from controllers.shipment import create_shipment, get_shipments_by_user
 
 shipment_blueprint = Blueprint("shipment", __name__)
-shipment_blueprint.route("/", methods=["POST"])(create_shipment)
-shipment_blueprint.route("/", methods=["GET"])(get_shipments_by_user)
+shipment_blueprint.route("", methods=["POST"])(create_shipment)
+shipment_blueprint.route("", methods=["GET"])(get_shipments_by_user)
